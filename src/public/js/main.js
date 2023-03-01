970acdf91eff6c628c6d8da145d06469d1823a40
Mélio <47474006+meliooff@users.noreply.github.com>
2023-03-02 00:24:03 +0100

    💄 Ajout d'un effet d'apparition

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -11,3 +11,16 @@ $(function() {
         }
     });
 });
+
+
+// REVEAL EFFECT
+const handleIntersect = function(entries, observer) {
+    entries.forEach(function(entry) {
+        if(entry.intersectionRatio > .1) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+        }
+    })
+}
+const observer = new IntersectionObserver(handleIntersect, { root: null, rootMargin: '0px', threshold: .1 });
+document.querySelectorAll('[class*="reveal"]').forEach(function(r) { observer.observe(r) });
