5e97a3885b25fb6474136948bcfcdf0ee39c03c0
Mélio <47474006+meliooff@users.noreply.github.com>
2023-03-04 15:45:26 +0100

    ✨ Ajout des médias (trailer & previews + slider)

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -42,3 +42,72 @@ $(document).ready(function() {
         return false;
     });
 });
+
+
+// MEDIA SLIDER
+const img_slider = document.getElementsByClassName('slider-img');
+let stage = 0; let img_amount = img_slider.length;
+const slider_previous = document.querySelector('.previous');
+const slider_next = document.querySelector('.next');
+const slider_pagination = document.querySelectorAll('.slider_pagination_bubbles span')
+
+function removeSliderActiveImages() {
+    for(let i=0; i<img_amount ; i++) {
+        img_slider[i].classList.remove('active');
+        slider_pagination.forEach(pag => pag.classList.remove('active'));
+    }
+}
+
+function showNextSliderImage() {
+    stage++;
+    if(stage >= img_amount) { stage = 0; }
+
+    removeSliderActiveImages();
+    img_slider[stage].classList.add('active');
+    slider_pagination[stage].classList.add('active');
+}
+slider_next.addEventListener('click', function() { showNextSliderImage() });
+
+slider_previous.addEventListener('click', function() {
+    stage--;
+    if(stage < 0) { stage = img_amount - 1; }
+
+    removeSliderActiveImages();
+    img_slider[stage].classList.add('active');
+    slider_pagination[stage].classList.add('active');
+});
+
+for(let i=0; i<slider_pagination.length ; i++) {
+    slider_pagination[i].addEventListener('click', () => {
+        showNextSliderImage();
+    })
+}
+// setInterval(function() { showNextSliderImage(); }, 4000)
+
+
+// MODAL OPENING MEDIA SLIDE
+const modal = document.querySelector('#modal');
+const closeModalButtons = document.querySelector('[data-modal-close]');
+
+function openSlider(position) {
+    stage = position;
+    removeSliderActiveImages()
+    img_slider[position].classList.add('active');
+    slider_pagination[position].classList.add('active');
+
+    if(modal === null) { return }
+    modal.classList.add('active');
+ 
+    $('body').addClass('no-scroll')
+}
+
+function closeSlider(modal) {
+    if(modal === null) { return }
+    stage = 0;
+    modal.classList.remove('active');
+    
+    $('body').removeClass('no-scroll');
+    window.location.href = window.location.origin + '#medias'
+};
+
+closeModalButtons.addEventListener('click', () => { closeSlider(modal); })
