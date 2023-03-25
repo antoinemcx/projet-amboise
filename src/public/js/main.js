f2d959fcad91e695a340395520eeb0119d3ddf59
Mélio <47474006+meliooff@users.noreply.github.com>
2023-03-25 23:32:35 +0100

    ✨ Menu hamburger mobile (+début responsive)

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,3 +1,24 @@
+// MOBILE HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__links");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
 // SCROLL DOWN
 $(function() {
     $('a[href*=#]').on('click', function(e) {
