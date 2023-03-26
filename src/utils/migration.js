4da9a2c1a05e9ae1d703afb93ba279fbc508eb80
Mélio <47474006+meliooff@users.noreply.github.com>
2023-03-26 22:40:20 +0200

    🗃️ Mise à jour du fichier de migration

--- src/utils/migration.js
+++ src/utils/migration.js
@@ -1,7 +1,18 @@
 const db = require('./database');
+// TODO: à vérifier
 
-db.query(`CREATE TABLE IF NOT EXISTS <tableName> (
-    //TODO: Quand la bdd est faite
+db.query(`CREATE TABLE IF NOT EXISTS \`users\` (
+    \`id\` varchar(100) PRIMARY KEY NOT NULL,
+    \`username\` varchar(50) NOT NULL,
+    \`email\` varchar(100) NOT NULL,
+    \`prenom\` varchar(100) NOT NULL,
+    \`nom\` varchar(100) NOT NULL,
+    \`password\` varchar(500) NOT NULL,
+    \`createdAt\` date NOT NULL DEFAULT current_timestamp(),
+    \`stm\` varchar(50) DEFAULT NULL,
+    \`offer\` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 `, (err) => {
     if (err) {
         console.log(err);
