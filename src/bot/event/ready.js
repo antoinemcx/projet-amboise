c4e1eff4d6ab4a8de4962c97188d00e2fa6c6e4e
Mélio <47474006+meliooff@users.noreply.github.com>
2023-03-30 14:22:20 +0200

    💄

--- src/bot/event/ready.js
+++ src/bot/event/ready.js
@@ -1,4 +1,4 @@
 module.exports = async (client) => {
     console.log("\x1b[33m%s\x1b[0m", `(!) ${client.user.username} est en ligne...`)
-    client.user.setPresence({ activities: [{ name: `${client.config.prefix}help • bot du serveur` }] })
+    client.user.setPresence({ activities: [{ name: `${client.config.prefix}about • bot du serveur` }] })
 };
